dc383dfd70156d1eee8253e4fee6d882ba57302c
Joseph Chiang <josephchiang28@gmail.com>
2016-03-01 13:47:06 -0800

    Generated new project in App

---
+++ App/Prog-02-represent/mobile/src/main/java/com/cs160/joseph/prog_02_represent/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cs160.joseph.prog_02_represent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
